db0c2a10b4956e704cf363038c8ecc3e87b021a8
Gerrit Grunwald <han.solo@mac.com>
2017-03-23 15:48:29 +0100

    Added json output to StockQuote

--- src/main/java/eu/hansolo/fx/tilesfxstockdemo/StockQuote.java
+++ src/main/java/eu/hansolo/fx/tilesfxstockdemo/StockQuote.java
@@ -5,6 +5,7 @@ import eu.hansolo.tilesfx.Tile.TileColor;
 import org.json.simple.JSONObject;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
@@ -17,24 +18,24 @@ public class StockQuote {
     private static final int               RED       = DotMatrix.convertToInt(TileColor.LIGHT_RED.color);
     private static final int               ORANGE    = DotMatrix.convertToInt(TileColor.ORANGE.color);
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zXXX yyyy", Locale.US);
-    private String        symbol;
-    private String        name;
-    private LocalDateTime timestamp;
-    private double        previousClose;
-    private double        open;
-    private double        lastPrice;
-    private double        high;
-    private double        low;
-    private double        change;
-    private double        changePercent;
-    private int           colorValue;
+    private              String            symbol;
+    private              String            name;
+    private              ZonedDateTime     timestamp;
+    private              double            previousClose;
+    private              double            open;
+    private              double            lastPrice;
+    private              double            high;
+    private              double            low;
+    private              double            change;
+    private              double            changePercent;
+    private              int               colorValue;
 
 
     // ******************** Constructor ***************************************
     public StockQuote(final JSONObject JSON_QUOTE) {
         symbol        = JSON_QUOTE.get("Symbol").toString();
         name          = JSON_QUOTE.get("Name").toString();
-        timestamp     = LocalDateTime.parse(JSON_QUOTE.get("Timestamp").toString().replace("UTC", "EDT"), FORMATTER);
+        timestamp     = ZonedDateTime.parse(JSON_QUOTE.get("Timestamp").toString().replace("UTC", "EDT"), FORMATTER);
         open          = Double.parseDouble(JSON_QUOTE.get("Open").toString());
         lastPrice     = Double.parseDouble(JSON_QUOTE.get("LastPrice").toString());
         high          = Double.parseDouble(JSON_QUOTE.get("High").toString());
@@ -47,7 +48,7 @@ public class StockQuote {
     public StockQuote(final String SYMBOL) {
         symbol        = SYMBOL;
         name          = "";
-        timestamp     = LocalDateTime.now();
+        timestamp     = ZonedDateTime.now();
         previousClose = 0;
         open          = 0;
         lastPrice     = 0;
@@ -65,8 +66,8 @@ public class StockQuote {
     public String getName() { return name; }
     public void setName(final String NAME) { name = NAME; }
 
-    public LocalDateTime getTimestamp() { return timestamp; }
-    public void setTimestamp(final LocalDateTime TIMESTAMP) { timestamp = TIMESTAMP; }
+    public ZonedDateTime getTimestamp() { return timestamp; }
+    public void setTimestamp(final ZonedDateTime TIMESTAMP) { timestamp = TIMESTAMP; }
 
     public double getPreviousClose() { return previousClose; }
     public void setPreviousClose(final double PREVIOUS_CLOSE) { previousClose = PREVIOUS_CLOSE; }
@@ -102,7 +103,7 @@ public class StockQuote {
     public void update(final JSONObject JSON_QUOTE) {
         symbol        = JSON_QUOTE.get("Symbol").toString();
         name          = JSON_QUOTE.get("Name").toString();
-        timestamp     = LocalDateTime.parse(JSON_QUOTE.get("Timestamp").toString().replace("UTC", "EDT"), FORMATTER);
+        timestamp     = ZonedDateTime.parse(JSON_QUOTE.get("Timestamp").toString().replace("UTC", "EDT"), FORMATTER);
         open          = Double.parseDouble(JSON_QUOTE.get("Open").toString());
         lastPrice     = Double.parseDouble(JSON_QUOTE.get("LastPrice").toString());
         high          = Double.parseDouble(JSON_QUOTE.get("High").toString());
@@ -112,4 +113,21 @@ public class StockQuote {
         previousClose = lastPrice - change;
         colorValue    = change < 0 ? RED : change > 0 ? GREEN : ORANGE;
     }
+
+    public JSONObject toJSON() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("Symbol", getSymbol());
+        jsonObject.put("Name", getName());
+        jsonObject.put("Timestamp", FORMATTER.format(getTimestamp()));
+        jsonObject.put("PreviousClose", getPreviousClose());
+        jsonObject.put("Open", getOpen());
+        jsonObject.put("LastPrice", getLastPrice());
+        jsonObject.put("High", getHigh());
+        jsonObject.put("Low", getLow());
+        jsonObject.put("Change", getChange());
+        jsonObject.put("ChangePercent", getChangePercent());
+        return jsonObject;
+    }
+
+    @Override public String toString() { return toJSON().toJSONString(); }
 }
